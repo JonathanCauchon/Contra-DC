67bb4c584326678558c96337ad8f917b54bf4c1f
Jonathan Cauchon <jnthncchn@gmail.com>
2019-09-23 11:07:26 -0400

    added 4d plot

--- neff_mapping.py
+++ neff_mapping.py
@@ -3,15 +3,13 @@ from Modules import *
 
 from mpl_toolkits.mplot3d import Axes3D
 
-n1 = np.reshape(np.loadtxt("Database/neff/neff_1.txt"),(5,5,5))
-n2 = np.reshape(np.loadtxt("Database/neff/neff_2.txt"),(5,5,5))
-w1, w2, wvl = np.loadtxt("Database/neff/w1_w2_lambda.txt")
+sims = np.loadtxt("Interpolation/indices.txt")
 
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-img = ax.scatter(w1*1e6, w2*1e6, wvl*1e9, c=n1[0,0,:],cmap=plt.hot())
+img = ax.scatter(sims[:,0]*1e6, sims[:,1]*1e6, sims[:,2]*1e9, c=sims[:,3],cmap=plt.hot())
 fig.colorbar(img)
 plt.xlabel("Width of wg1 (um)")
 plt.ylabel("Width of wg2 (um)")
