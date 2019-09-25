b09adc040fea2bad32f45819e6b8bdafc17388e4
Jonathan Cauchon <jnthncchn@gmail.com>
2019-09-25 13:40:33 -0400

    Functional

--- main.py
+++ main.py
@@ -20,37 +20,33 @@ from ChirpedContraDC_v1 import *
 
 
 # Chirp
-w1 = [.55e-6, .57e-6]
-w2 = [.45e-6, .44e-6]
+w1 = [.555e-6, .56e-6]
+w2 = [.443e-6, .448e-6]
 w1_ = .55e-6
 w2_ = .43e-6
-p  = [320e-9, 324e-9]
+p  = [312e-9, 338e-9]
 p_ = 324e-9
 wr = [1500e-9,1700e-9]
 wr_ = [1530e-9,1600e-9]
 
-# d = ChirpedContraDC(w1=.56e-6,w2=.44e-6,resolution=3,a=5,period=324e-9,N=1000, wvl_range=wr, N_seg = 50)
-# d = ChirpedContraDC(a=5, w1=w1, w2=w2_, period=p_, resolution = 200, N_seg = 50, wvl_range=wr_)
-# d.getApodProfile()
-# d.getChirpProfile(plot=False)
-# d.getPropConstants(plot=False)
-# d.propagate()
-# d.displayResults()
+d = ChirpedContraDC(N=3800, a=5, w1=w1_, w2=w2_, period=p, resolution = 500, N_seg = 100, wvl_range=wr)
+d.simulate()
+d.displayResults()
 
-standard = ChirpedContraDC(period=330e-9, resolution=100, wvl_range=[1530e-9,1575e-9])
-standard.simulate()
-standard.getPerformance()
+# standard = ChirpedContraDC(period=324e-9, resolution=200, wvl_range=[1550e-9,1600e-9])
+# standard.simulate()
+# standard.getPerformance()
 # standard.displayResults()
-wvl_0 = standard.performance[0][1] # The basic reflection wavelength
+# wvl_0 = standard.performance[0][1] # The basic reflection wavelength
 
 
-ff = [1, 1, 1, 1]
+ff = [0, 0, 0, 0, 0]
 
 if ff[0]:
 	periods = np.arange(310e-9,330e-9,2e-9)
 	lam = np.zeros((periods.size,1))
-	cdc = standard
-	cdc.wvl_range = [1500e-9,1600e-9]
+	cdc = ChirpedContraDC(period=324e-9, resolution=100, wvl_range=[1500e-9,1600e-9])
+	cdc.wvl_range = [1500e-9,1620e-9]
 	for i in range(periods.size):
 		cdc.period = periods[i]
 		cdc.simulate()
@@ -68,7 +64,7 @@ if ff[0]:
 if ff[1]:
 	w1s = np.arange(.55e-6,.57e-6,2e-9)
 	lam = np.zeros((w1s.size,1))
-	cdc = standard
+	cdc = ChirpedContraDC(period=324e-9, resolution=100, wvl_range=[1550e-9,1580e-9])
 	for i in range(w1s.size):
 		cdc.w1 = w1s[i]
 		cdc.simulate()
@@ -86,7 +82,7 @@ if ff[1]:
 if ff[2]:
 	w2s = np.arange(.43e-6,.45e-6,2e-9)
 	lam = np.zeros((w2s.size,1))
-	cdc = standard
+	cdc = ChirpedContraDC(period=324e-9, resolution=100, wvl_range=[1550e-9,1580e-9])
 	for i in range(w2s.size):
 		cdc.w2 = w2s[i]
 		cdc.simulate()
@@ -102,10 +98,11 @@ if ff[2]:
 
 
 if ff[3]:
-	w1s = np.arange(.55e-6,.57e-6,2e-9)
-	w2s = np.arange(.43e-6,.45e-6,2e-9)
+	w1s = np.arange(.55e-6,.571e-6,2e-9)
+	w2s = np.arange(.43e-6,.451e-6,2e-9)
+	dw = w2s-0.44e-6
 	lam = np.zeros((w2s.size,1))
-	cdc = standard
+	cdc = ChirpedContraDC(period=324e-9, resolution=100, wvl_range=[1550e-9,1580e-9])
 	for i in range(w2s.size):
 		cdc.w1 = w1s[i]
 		cdc.w2 = w2s[i]
@@ -114,8 +111,20 @@ if ff[3]:
 		lam[i] = cdc.performance[0][1]
 
 	plt.figure()
-	plt.plot((w2s-0.44e-6)*1e6,lam,"o-")
-	plt.xlabel("Width Detuning (um)")
+	plt.plot(dw*1e9,lam,"o-")
+	plt.xlabel("Width Detuning (nm)")
 	plt.ylabel("Centre Reflection Wavelength (nm)")
 	plt.savefig("Plots/centre_wvl_vs_w1w2.pdf")
 	plt.show()
+
+
+
+
+if ff[4]:
+	# Contruction of a custom w1w2 chirp profile
+	w1 = [.54e-6, .57e-6]
+	w2 = [.43e-6, .45e-6]
+	p = []
+
+
+
