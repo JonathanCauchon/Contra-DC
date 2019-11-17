7b4fc3bb0c92ab22796ef3f0de69be14a456c0e2
Jonathan Cauchon <jnthncchn@gmail.com>
2019-11-17 12:41:48 -0500

    s

--- Design_ANT_November.py
+++ Design_ANT_November.py
@@ -7,8 +7,21 @@ from Auto_design import autoDesign
 # Intended designs
 center_wvl = (1610+1530)*1e-9/2
 
-BWs = np.arange(15, 125, 5)*1e-9
+# BWs = np.arange(15, 125, 5)*1e-9
+# BWs = np.arange(15, 125, 5)*1e-9
+BWs = np.arange(20, 75, 10)*1e-9
+
+""" Fabricated devices
+- 6 targeted BWs
+- 3-4 N1-N2-Nin variations
+- 3-4 wavelength offsets for experimental ref wvl uncertainty
+
+- single stage forward, backward,  dual-stage same dir., alternated
+
+"""
+
 print(BWs)
+
 
 # BWs = np.array([10e-9, 30e-9])
 target_wvls = np.transpose([center_wvl + BWs/2, center_wvl - BWs/2])
